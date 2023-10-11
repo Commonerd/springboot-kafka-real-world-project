79912ee67db0923243d9eddb9761f879e0d8c7ff
[Commonerd] <[syh712@gmail.com]>
2023-10-11 14:37:02 +0900

    feat: Add wikimedia consumer project setup

---
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/SpringBootConsumerApplication.java
@@ -0,0 +1,15 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class SpringBootConsumerApplication {
+
+    public static void main(String[] agrs) {
+
+        SpringApplication.run(SpringBootConsumerApplication.class);
+    }
+}
